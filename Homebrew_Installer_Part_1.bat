b30cef2175fd9265c261278288052018f122ccfe
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-01-04 13:22:48 -0600

    Add files via upload

--- Homebrew_Installer_Part_1.bat
+++ Homebrew_Installer_Part_1.bat
@@ -1,5 +1,7 @@
 C:
 mkdir homebrew
+cd C:\homebrew
+curl https://github.com/Mattamech/Wii-U-Homebrew-Installer/releases/download/1/Homebrew_Installer_Part_2.bat --output Homebrew_Installer_Part_2.bat
 pause
 start C:\homebrew\Homebrew_Installer_Part_2.bat
 exit

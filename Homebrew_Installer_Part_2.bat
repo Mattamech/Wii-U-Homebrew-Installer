a52e16b931280584f3723de859d4076fe85caa42
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-01-04 13:53:22 -0600

    Updates

--- Homebrew_Installer_Part_2.bat
+++ Homebrew_Installer_Part_2.bat
@@ -1,8 +1,8 @@
 curl https://github.com/dimok789/homebrew_launcher/releases/download/v1.3/homebrew_launcher.v1.3.zip --output homebrew_launcher.zip
 curl https://wiiubru.com/appstore/zips/appstore.zip --output appstore.zip
-curl https://wiiubru.com/appstore/zips/wuphax.zip --output wuphax.zip
 curl https://wiiubru.com/appstore/zips/mocha_fshax.zip --output mocha.zip
-curl https://bootmii.org/get.php?file=hackmii_installer_v1.2.zip&key=51560c13f240e84fc9d3866c5b128d061fdfd657 --output hackmii_istaller.zip
+curl https://wiiubru.com/appstore/zips/haxchi.zip --output haxchi.zip
 curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
 pause
+start Homebrew_Installer_Part_3.bat
 exit

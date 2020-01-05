7895a69684be099ed259cf9790e038bf5e744632
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-01-05 07:16:32 -0600

    Update Homebrew_Installer_Part_2.bat

--- Homebrew_Installer_Part_2.bat
+++ Homebrew_Installer_Part_2.bat
@@ -1,3 +1,4 @@
+cd C:\homebrew\sd
 curl https://github.com/dimok789/homebrew_launcher/releases/download/v1.3/homebrew_launcher.v1.3.zip --output homebrew_launcher.zip
 curl https://wiiubru.com/appstore/zips/appstore.zip --output appstore.zip
 curl https://wiiubru.com/appstore/zips/mocha_fshax.zip --output mocha.zip
@@ -5,5 +6,5 @@ curl https://wiiubru.com/appstore/zips/haxchi.zip --output haxchi.zip
 curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
 curl https://raw.githubusercontent.com/Mattamech/Wii-U-Homebrew-Installer/master/Homebrew_Installer_Part_3.bat --output Homebrew_Installer_Part_3.bat
 pause
-start C:\homebrew\Homebrew_Installer_Part_3.bat
+start C:\homebrew\\sd\Homebrew_Installer_Part_3.bat
 exit

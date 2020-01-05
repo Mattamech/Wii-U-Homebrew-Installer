771f99802332159986043ef792c31c8a23a95dc2
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-01-05 07:47:23 -0600

    Update Homebrew_Installer_Part_4.bat

--- Homebrew_Installer_Part_4.bat
+++ Homebrew_Installer_Part_4.bat
@@ -3,6 +3,6 @@ del /f *.exe
 del /f *.bat
 pause
 cd C:\homebrew
-curl (Part 5)
+curl https://raw.githubusercontent.com/Mattamech/Wii-U-Homebrew-Installer/master/Homebrew_Installer_Part_5.txt --output Homebrew_Installer_Part_5.txt
 start C:\homebrew\Homebrew_Installer_Part_5.txt
 exit

b6c83d4d7fb93e48f583eb792492029a5c593678
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-04-11 06:32:03 -0600

    Delete Homebrew_Installer_Part_3.bat

--- Homebrew_Installer_Part_3.bat
+++
@@ -1,9 +0,0 @@
-unzip *zip
-pause
-del /f *.zip
-del /f *.json
-del /f *.install
-cd C:\homebrew
-curl https://raw.githubusercontent.com/Mattamech/Wii-U-Homebrew-Installer/master/Homebrew_Installer_Part_4.bat --output Homebrew_Installer_Part_4.bat
-start C:\homebrew\Homebrew_Installer_Part_4.bat
-exit

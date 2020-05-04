a251fdb017fc018981211f6c4c68879a2df4839b
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-05-04 09:17:32 -0600

    fix

---
+++ Version 2/Homebrew_Downloader.bat
@@ -0,0 +1,30 @@
+C:
+cd C:\
+mkdir homebrew
+cd C:\homebrew
+mkdir sd
+pause
+cd C:\homebrew\sd
+curl https://github.com/dimok789/homebrew_launcher/releases/download/v1.3/homebrew_launcher.v1.3.zip --output homebrew_launcher.zip
+curl https://wiiubru.com/appstore/zips/appstore.zip --output appstore.zip
+curl https://wiiubru.com/appstore/zips/mocha_fshax.zip --output mocha.zip
+curl https://wiiubru.com/appstore/zips/haxchi.zip --output haxchi.zip
+curl https://wiiubru.com/appstore/zips/cbhc.zip --output cbhc.zip
+curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
+pause
+unzip *zip
+pause
+del /f *.zip
+del /f *.json
+del /f *.install
+del /f *.png
+cd C:\homebrew
+pause
+cd C:\homebrew\sd
+del /f *.exe
+pause
+cd C:\homebrew
+curl https://raw.githubusercontent.com/Mattamech/Wii-U-Homebrew-Installer/Version-2/Homebrew_Installer.txt --output Homebrew_Installer.txt
+start C:\homebrew
+start C:\homebrew\Homebrew_Installer.txt
+exit
---
+++ Version 2/Homebrew_deleter_optional.bat
@@ -0,0 +1,2 @@
+RD /S /Q C:\homebrew
+exit

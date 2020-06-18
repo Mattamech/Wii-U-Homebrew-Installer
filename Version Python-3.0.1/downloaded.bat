a308f51b66d17f626788985c567de5176c4d39d5
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-06-18 11:34:18 -0500

    Create downloaded.bat

---
+++ Version Python-3.0.1/downloaded.bat
@@ -0,0 +1,6 @@
+curl https://github.com/dimok789/homebrew_launcher/releases/download/v1.3/homebrew_launcher.v1.3.zip --output homebrew_launcher.zip
+curl https://wiiubru.com/appstore/zips/appstore.zip --output appstore.zip
+curl https://wiiubru.com/appstore/zips/mocha_fshax.zip --output mocha.zip
+curl https://wiiubru.com/appstore/zips/haxchi.zip --output haxchi.zip
+curl https://wiiubru.com/appstore/zips/cbhc.zip --output cbhc.zip
+curl http://stahlworks.com/dev/unzip.exe --output unzip.exe

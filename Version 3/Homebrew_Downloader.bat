0a620ad17658058346f3fac5cd80c802bd993c4b
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-06-18 20:12:15 -0600

    Update Homebrew_Downloader.bat

--- Version 3/Homebrew_Downloader.bat
+++ Version 3/Homebrew_Downloader.bat
@@ -5,7 +5,7 @@ cd C:\homebrew
 mkdir sd
 pause
 cd C:\homebrew\sd
-curl https://github.com/dimok789/homebrew_launcher/releases/download/v1.3/homebrew_launcher.v1.3.zip --output homebrew_launcher.zip
+curl http://wiiubru.com/appstore/zips/homebrew_launcher.zip --output homebrew_launcher.zip
 curl https://wiiubru.com/appstore/zips/appstore.zip --output appstore.zip
 curl https://wiiubru.com/appstore/zips/mocha_fshax.zip --output mocha.zip
 curl https://wiiubru.com/appstore/zips/haxchi.zip --output haxchi.zip

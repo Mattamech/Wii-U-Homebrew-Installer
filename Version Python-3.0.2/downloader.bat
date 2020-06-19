c79bae93f39179dbffed83cfa4e333aa3da425e4
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-06-18 19:41:00 -0600

    Update downloader.bat

--- Version Python-3.0.2/downloader.bat
+++ Version Python-3.0.2/downloader.bat
@@ -1,4 +1,4 @@
-curl https://mattamech.github.io/Wii-U-Homebrew-Installer/docs/homebrew_launcher.v.1.4.zip --output homebrew_launcher.v.1.4.zip
+curl http://wiiubru.com/appstore/zips/homebrew_launcher.zip --output homebrew_launcher.zip
 curl https://wiiubru.com/appstore/zips/appstore.zip --output appstore.zip
 curl https://wiiubru.com/appstore/zips/mocha_fshax.zip --output mocha.zip
 curl https://wiiubru.com/appstore/zips/haxchi.zip --output haxchi.zip

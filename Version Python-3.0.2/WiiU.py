45c0af52941f922a06eb2ea35a2fee758c42a0f0
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-06-18 18:32:06 -0600

    Create Python-3.0.2

---
+++ Version Python-3.0.2/WiiU.py
@@ -0,0 +1,28 @@
+import wget
+from shutil import copyfile
+import os
+import shutil
+
+Download_Location = input("Enter folder here:")
+os.chdir(Download_Location)
+print(os.getcwd())
+input("If the folder above is not correct you will have to restart. Press enter to continue.")
+
+wget.download('https://mattamech.github.io/Wii-U-Homebrew-Installer/docs/downloader.bat', 'downloader.bat')
+
+input("Double Check that downloader.bat is now in the folder you specifyed. Press enter to continue.")
+
+os.system('cmd /c downloader.bat')
+os.system('cmd /c unzip *zip')
+os.system('cmd /c del /f *.zip')
+os.system('cmd /c del /f *.install')
+os.system('cmd /c del /f *.png')
+os.system('cmd /c del /f *.json')
+os.system('cmd /c del /f *.exe')
+os.system('cmd /c del /f *.bat')
+
+Save_Location = input("Enter a folder NOT created yet (EX: copy_to_sd) here:")
+shutil.copytree(Download_Location, Save_Location)
+
+input("If you followed the directions correctly the files should be in the root of the sd card. Press enter to exit.")
+exit()
---
+++ Version Python-3.0.2/copyer.bat
@@ -0,0 +1,9 @@
+mkdir copy_to_sd
+cd copy_to_sd
+mkdir cbhc
+mkdir kaxchi
+mkdir wiiu
+cd ../
+Xcopy /E /I cbhc \copy_to_sd\cbhc
+Xcopy /E /I haxchi \copy_to_sd\haxchi
+Xcopy /E /I wiiu \copy_to_sd\wiiu

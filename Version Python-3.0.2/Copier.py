13ff595402ac91ebe654eb7f302cf254b51fed28
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-06-18 20:03:24 -0600

    Update Copier.py

--- Version Python-3.0.2/Copier.py
+++ Version Python-3.0.2/Copier.py
@@ -1,10 +1,14 @@
 import shutil
 import os
 
-Download_Location = input("Enter folder with copy_to_sd folder here:")
+Download_Location = input("Enter folder with downloaded files here:")
 os.chdir(Download_Location)
 print(os.getcwd())
 input("If the folder above is not correct you will have to restart. Press enter to continue.")
+
+shutil.copytree('cbhc', 'copy_to_sd\cbhc')
+shutil.copytree('haxchi', 'copy_to_sd\haxchi')
+shutil.copytree('wiiu', 'copy_to_sd\wiiu')
 
 Save_Location = input("Enter Drive letter (ex G:) here:")
 shutil.move('copy_to_sd\cbhc', Save_Location)

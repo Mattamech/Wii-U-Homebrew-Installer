52b9b10d638c548bf71d4116df51d9943e8e7f8d
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-10-28 10:31:58 -0600

    Update WiiU.py

--- Version Python-3.2/WiiU.py
+++ Version Python-3.2/WiiU.py
@@ -55,7 +55,7 @@ if run_copier == "2":
     delete.write('cd ')
     delete.write(Download_Location)
     delete.write('\n')
-    delete.write('RD /S /Q copy-to-sd')
+    delete.write('RD /S /Q copy_to_sd')
     delete.close()
     os.system('cmd /c Delete.bat')
     os.system('cmd /c del /f Delete.bat')

69a9d00d3c33f5d7eac03819eb468882e4681a58
Lord-Giganticus <mrubins64@gmail.com>
2021-02-08 14:05:59 -0600

    Start Experimental C# version

---
+++ Version C#-1.0.0/Wii U Homebrew Installer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wii_U_Homebrew_Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

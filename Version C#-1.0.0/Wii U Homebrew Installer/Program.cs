1edbc4f12ef14a40d91778ad1f5f8741e44e7279
Lord-Giganticus <mrubins64@gmail.com>
2021-02-09 15:24:21 -0600

    Finish Download/Extrct section.

--- Version C#-1.0.0/Wii U Homebrew Installer/Program.cs
+++ Version C#-1.0.0/Wii U Homebrew Installer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System.Net;
+using System.Diagnostics;
 
 namespace Wii_U_Homebrew_Installer
 {
@@ -6,7 +8,22 @@ namespace Wii_U_Homebrew_Installer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var client = new WebClient())
+                client.DownloadFile("http://wiiubru.com/appstore/zips/homebrew_launcher.zip", "homebrew_launcher.zip");
+            using (var client = new WebClient())
+                client.DownloadFile("https://wiiubru.com/appstore/zips/appstore.zip", "appstore.zip");
+            using (var client = new WebClient())
+                client.DownloadFile("https://www.wiiubru.com/appstore/zips/mocha.zip", "mocha.zip");
+            using (var client = new WebClient())
+                client.DownloadFile("https://wiiubru.com/appstore/zips/haxchi.zip", "haxchi.zip");
+            using (var client = new WebClient())
+                client.DownloadFile("https://wiiubru.com/appstore/zips/cbhc.zip", "cbhc.zip");
+            using (var client = new WebClient())
+                client.DownloadFile("https://mattamech.github.io/Wii-U-Homebrew-Installer/cs/Haxchi-Installer.zip", "Haxchi-Installer.zip");
+            using (var client = new WebClient())
+                client.DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe");
+            Process.Start("CMD.exe", "unzip *zip & del /f *.zip & del /f *.install & del /f *.png & del /f *.json & del /f *.exe");
+            FileSystem.MkDir("Copy_to_SD");
         }
     }
 }

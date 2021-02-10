11e93d01853cf4eaba46905d7cc5b710fe189a58
Lord-Giganticus <mrubins64@gmail.com>
2021-02-10 08:20:18 -0600

    Remove del /f unzip.exe for later usage.

--- docs/cs/Extract.bat
+++ docs/cs/Extract.bat
@@ -1,1 +1,1 @@
-unzip *zip & del /f *.zip & del /f *.install & del /f *.png & del /f *.json & del /f unzip.exe & RD /S /Q haxchi
+unzip *zip & del /f *.zip & del /f *.install & del /f *.png & del /f *.json & RD /S /Q haxchi

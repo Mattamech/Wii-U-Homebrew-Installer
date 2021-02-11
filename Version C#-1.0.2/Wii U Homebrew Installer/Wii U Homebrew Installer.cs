6a3ac0bbd4d3cf6783f616139f7e73c30088b6d7
Lord-Giganticus <mrubins64@gmail.com>
2021-02-10 22:20:58 -0600

    update use of WebClient().DownloadFile()

--- Version C#-1.0.2/Wii U Homebrew Installer/Wii U Homebrew Installer.cs
+++ Version C#-1.0.2/Wii U Homebrew Installer/Wii U Homebrew Installer.cs
@@ -21,23 +21,18 @@ namespace Wii_U_Homebrew_Installer
             }
             Console.WriteLine("Downloading files.");
             using (var client = new WebClient())
+            {
                 client.DownloadFile("http://wiiubru.com/appstore/zips/homebrew_launcher.zip", "homebrew_launcher.zip");
-            using (var client = new WebClient())
                 client.DownloadFile("https://wiiubru.com/appstore/zips/appstore.zip", "appstore.zip");
-            using (var client = new WebClient())
                 client.DownloadFile("https://www.wiiubru.com/appstore/zips/mocha.zip", "mocha.zip");
-            using (var client = new WebClient())
                 client.DownloadFile("https://wiiubru.com/appstore/zips/haxchi.zip", "haxchi.zip");
-            using (var client = new WebClient())
                 client.DownloadFile("https://wiiubru.com/appstore/zips/cbhc.zip", "cbhc.zip");
-            using (var client = new WebClient())
                 client.DownloadFile("https://mattamech.github.io/Wii-U-Homebrew-Installer/cs/Haxchi-Installer.tar.gz", "Haxchi-Installer.tar.gz");
-            using (var client = new WebClient())
                 client.DownloadFile("https://mattamech.github.io/Wii-U-Homebrew-Installer/cs/Extract.bat", "Extract.bat");
-            using (var client = new WebClient())
                 client.DownloadFile("https://mattamech.github.io/Wii-U-Homebrew-Installer/cs/Move.bat", "Move.bat");
-            using (var client = new WebClient())
                 client.DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe");
+                client.DownloadFile("http://stahlworks.com/dev/tar.exe", "tar.exe");
+            }
             Directory.CreateDirectory("Copy_to_SD");
             Environment.CurrentDirectory = Directory.GetCurrentDirectory();
             Console.WriteLine("Running .bat files.");
